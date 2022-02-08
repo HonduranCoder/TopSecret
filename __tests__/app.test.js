4c4a715d912e4c71e490a28a56bb53f4c72c779e
Estefani <stef45689@gmail.com>
2022-02-08 10:37:32 -0600

    setup and create user test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,11 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
 describe('backend routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +15,14 @@ describe('backend routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+  it('creates a new user with hashed password', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
 });
