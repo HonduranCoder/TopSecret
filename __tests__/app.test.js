ec27f65544c085cf53b74c0002f57f6aa162ef1f
Estefani <stef45689@gmail.com>
2022-02-08 12:11:40 -0600

    login w/test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,20 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 const mockUser = {
   email: 'test@example.com',
   password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('backend routes', () => {
@@ -23,6 +33,17 @@ describe('backend routes', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
+    });
+  });
+  //response = session object: id, email, token
+  it('logs in a user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(mockUser);
+
+    expect(res.body).toEqual({
+      message: 'Signed in successfully!',
     });
   });
 });
