ecb549b4329d0ec2523cf995174da923afa71752
Estefani <stef45689@gmail.com>
2022-02-08 17:51:16 -0600

    two tests because we got help

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Secret = require('../lib/models/Secret.js');
 const UserService = require('../lib/services/UserService.js');
+const req = require('express/lib/request');
 
 const mockUser = {
   email: 'test@example.com',
   password: '12345',
 };
+
+const agent = request.agent(app);
 
 const registerAndLogin = async (userProps = {}) => {
   const password = userProps.password ?? mockUser.password;
@@ -45,5 +49,43 @@ describe('backend routes', () => {
     expect(res.body).toEqual({
       message: 'Signed in successfully!',
     });
+  });
+  it('logs a user out', async () => {
+    const res = await agent.delete('/api/v1/users/sessions').send(mockUser);
+
+    expect(res.body).toEqual({
+      success: true,
+      message: 'Signed out successfully!',
+    });
+  });
+  //test for creating a secret
+  it('create a secret', async () => {
+    const [agent, user] = await registerAndLogin();
+    const res = await agent
+      .post('/api/v1/secrets')
+      .send({ title: 'secret', description: 'secret, secret' });
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      title: 'secret',
+      description: 'secret, secret',
+    });
+  });
+  it('list of secrets if signed in', async () => {
+    const [agent, user] = await registerAndLogin();
+    await agent.post('/api/v1/secrets').send({
+      title: 'secret',
+      description: 'secret, secret',
+    });
+
+    const res = await agent.get('/api/v1/secrets');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        title: 'secret',
+        description: 'secret, secret',
+      },
+    ]);
   });
 });
