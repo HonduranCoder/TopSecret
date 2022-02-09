a6890a0d0d682a71d866ef69541c80016b386d32
Estefani <76499574+HonduranCoder@users.noreply.github.com>
2022-02-08 18:08:54 -0600

    Update app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Secret = require('../lib/models/Secret.js');
 const UserService = require('../lib/services/UserService.js');
-const req = require('express/lib/request');
 
 const mockUser = {
   email: 'test@example.com',
